0bf0b4cccbc1d0d94c574e992892e5e0c88552b1
crisilto <crisilto1997@gmail.com>
2024-07-19 19:39:03 +0200

    first commit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,3 +4,23 @@
  * Clicking on any individual square should change the color of that individual square to red. 
  * Double click on any of the squares should change all of them to blue.
  */
+
+import React, { useCallback, useState } from 'react';
+import Square from './components/Square';
+
+const App: React.FC = () => {
+  const [key, setKey] = useState(0);
+  const resetColors = useCallback(() => {
+    setKey(prevKey => prevKey + 1);
+  }, []);
+
+  return(
+    <div style={{display: "grid", gridTemplateColumns: "repeat(3, 50px)"}}>
+      {Array.from({length: 9}).map((_, index) => (
+        <Square key={`${key}-${index}`} onDoubleClick={resetColors}/>
+      ))}
+    </div>
+  )
+}
+
+export default App;
