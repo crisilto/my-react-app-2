cd1e83d5c1d03968c7c1a722c9832bfd39bf1306
crisilto <crisilto1997@gmail.com>
2024-07-19 11:50:09 +0200

    doing the exercise with js vanilla

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Vite + React!</h1>
-    </div>
-  );
-}
-
-export default App;
+/**
+ * Create a 3x3 set of 9 squares in a square formation. 
+ * All of these squares should be blue. 
+ * Clicking on any individual square should change the color of that individual square to red. 
+ * Double click on any of the squares should change all of them to blue.
+ */
